8ffb66283835102d1baa5ebeeb544e4617c86e7b
Yakoub <yakoubbeneyad@gmail.com>
2026-08-24 02:08:31 +0200

    adjustments in hyprland.lua and package installation in install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,17 @@
 #!/usr/bin/bash
 sudo -v
 
-sudo pacman -Syu
-
-sudo pacman -Ss fisher
+sudo pacman -Syu hyprland nvim waybar fish awww kitty tmux nemo grimblast rofi nm-applet hyprpolkitagent base-devel hyprlock gtk3 gtk4 qt6ct 
+echo ""
+read -p "Install yay? <y/N> " prompt
+if [[ $prompt == "y" || $prompt == "Y" || $prompt == "yes" || $prompt == "Yes" ]]
+then
+  	git clone https://aur.archlinux.org/yay.git
+	makepkg -si 
+	yay -S bibata-cursor-theme-bin xembedsniproxy
+else
+  exit 0
+fi
 
 
 
